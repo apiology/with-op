48a214c9565a8c98729049f2b5d655ef7030a6b4
Vince Broz <vince@broz.cc>
2022-05-04 21:22:44 -0400

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -132,6 +132,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/apiology/with-op',
-    version='2.0.0',
+    version='2.0.1',
     zip_safe=False,
 )
